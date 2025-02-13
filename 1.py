eb3324abf60a70649f5a753d2c0ee3fa601714a1
Kdjhfejd <100012690+Kdjhfejd@users.noreply.github.com>
2025-02-13 13:33:07 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ st.write("Welcome to the Education Platform. Select a course below:")
 courses = {
     "Railway science foundation": "https://t.me/+Ja5IJyCmnMVkM2Vl",
     "CGL +  CHSL + MTS MAINS": "https://t.me/+DumAzg8tD3I5NDc1",
-    "Machine Learning Basics": "https://example.com/ml",
+    "Machine Learning Basics": "https://t.me/+Q3WIIs6Oo6NjNjVl",
     "Web Development": "https://example.com/webdev"
 }
 st.image("home.jpg", caption="Welcome to the Education Platform", use_column_width=True)
@@ -20,4 +20,4 @@ for course, link in courses.items():
 st.write("### 📢 Connect with us on Social Media")
 st.markdown("[📢 Join our Telegram](https://t.me/+XuIEiecTujMxODNl)")
 st.markdown("[ Follow us on Whatsapp](https://whatsapp.com/channel/0029Vb25egi7oQhbkaBNse2o)")
-st.markdown("[📷 Follow us on Instagram](https://instagram.com/example)")
+st.markdown("[📷 Follow us on Instagram](https://www.instagram.com/mkraajltp1?utm_source=qr&igsh=MWxicHRkaWk2bnJmdA==)")
